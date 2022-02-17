52a4e920a19da6cc572cd6554293467b40b74a02
unknown <eugeneamo85@gmail.com>
2022-02-17 16:29:54 +0000

    done with the video footer

--- src/video.js
+++ src/video.js
@@ -24,7 +24,8 @@ function Video() {
         ref={videoRef}
         onClick={onVideoPressed}
         loop
-        src='https://v16-webapp.tiktok.com/3ee1155725f1ba71bd580d2656344dce/620dcbb2/video/tos/useast2a/tos-useast2a-pve-0068/e554078146c9413984f6d77b8e55f73f/?a=1988&br=4768&bt=2384&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3ydnz7ThSTPEDXq&l=20220216221430010192163163197C9AEF&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mztkbzc6ZjtvOzMzNzczM0ApaGlnZmgzNzs7N2dkZ2RpaWdkZzFecjRnbTFgLS1kMTZzc2JjLTYxYDQzLzAwMTY0YWI6Yw%3D%3D&vl=&vr='>
+        src='https://v16-webapp.tiktok.com/47f0c6590d1303d2e102c9b5157dd0f6/620ec7ff/video/tos/useast2a/tos-useast2a-ve-0068c004/79cb7b58c64b438384f842f9e3b12898/?a=1988&br=4720&bt=2360&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3ydnz7ThReCEDXq&l=202202171610070101902092291E3786E2&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2k6bWk6ZmpnOzMzNzczM0ApZzg4M2U2NmU3NztoZ2ZpZmdmXmFlcjQwMS5gLS1kMTZzczYtL2JhMmMxXzU0L19eXl46Yw%3D%3D&vl=&vr='
+        >
 
         </video>
         
--- src/videoFooter.js
+++ src/videoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./videofooter.css"
+import {Chat, ChatBubble, MusicNote, MusicNoteIcon} from "@material-ui/icons";
+import Ticker from "react-ticker";
 
 function VideoFooter() {
   return (
@@ -9,11 +11,26 @@ function VideoFooter() {
         <div className='footer__text'>
             <h3>@Genicist</h3> 
             <h5>Description</h5>
+            <div className="footer__ticker">
+                <MusicNote className='footer__icon'/>
+
+                <Ticker mode="smooth" className="ticker">
+                    {({index})=>(
+                        <>
+                        <p>hello this is from genics</p>
+                        </>
+                    )}
+
+                </Ticker>
+
+            </div>
+
+            
         </div>
         
         <img 
             className='footer__image'
-            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzkE5MYBMroeHOkynCzljWhjvPG6e7-gs9wQ&usqp=CAU'
+            src='https://image.pngaaa.com/178/1590178-small.png'
              alt=''
         />
             
